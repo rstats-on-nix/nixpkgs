a4fd54488d1009eea949e87bdcd06f0d0a31699f
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-04-16 17:33:00 +0200

    Update cachix.nix

--- cachix.nix
+++ cachix.nix
@@ -4,7 +4,7 @@ let
   inherit (pkgs) R quarto glibcLocalesUtf8 nix;
 };
  r_packages = builtins.attrValues {
-  inherit (pkgs.rPackages) tidyverse duckdb VariantAnnotation qeML rJava data_table readxl fixest collapse rstanarm gdalcubes sf stars vapour quarto Rcpp terra geos devtools fledge fusen codetools jsonlite httr sys testthat knitr stringi blavaan gpboost igraph rmarkdown;
+  inherit (pkgs.rPackages) tidyverse duckdb VariantAnnotation s2 qeML rJava data_table readxl fixest collapse rstanarm gdalcubes sf stars vapour quarto Rcpp terra geos devtools fledge fusen codetools jsonlite httr sys testthat knitr stringi blavaan gpboost igraph rmarkdown;
 };
   tex = (pkgs.texlive.combine {
   inherit (pkgs.texlive) scheme-small;
