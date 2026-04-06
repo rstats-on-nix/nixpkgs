045461674998b849fbd3348ce1ecb5344de86ac8
Bruno Rodrigues <brodriguesco@protonmail.com>
2026-04-06 11:03:14 +0200

    python3Packages.pypmml: init at 1.5.8

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -14814,6 +14814,8 @@ self: super: with self; {
 
   pyplatec = callPackage ../development/python-modules/pyplatec { };
 
+  pypmml = callPackage ../development/python-modules/pypmml { };
+
   pypng = callPackage ../development/python-modules/pypng { };
 
   pypoint = callPackage ../development/python-modules/pypoint { };
