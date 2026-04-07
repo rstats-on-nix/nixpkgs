6692de60216eedb6f921dfcfb77c20022efe6536
Bruno Rodrigues <brodriguesco@protonmail.com>
2026-04-07 12:49:23 +0200

    added java jpmml evaluator

---
+++ pkgs/by-name/jp/jpmml-evaluator/package.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  maven,
+  fetchFromGitHub,
+  makeBinaryWrapper,
+  jre_headless,
+  nix-update-script,
+}:
+
+let
+  pname = "jpmml-evaluator";
+  version = "1.7.7";
+in
+maven.buildMavenPackage {
+  inherit pname version;
+
+  src = fetchFromGitHub {
+    owner = "jpmml";
+    repo = "jpmml-evaluator";
+    rev = version;
+    hash = "sha256-DtI/cHmiKVH0IAp3mWJr2sDDjAzM5d9/cBx4KJm74WM=";
+  };
+
+  mvnHash = "sha256-7HabnY9lxQTc7XjXEMbaQk23UGR4y7pbTsPOTTefT3E=";
+
+  nativeBuildInputs = [
+    makeBinaryWrapper
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/java $out/bin
+    cp pmml-evaluator-example/target/pmml-evaluator-example-executable-*.jar $out/share/java/jpmml-evaluator.jar
+
+    makeBinaryWrapper ${jre_headless}/bin/java $out/bin/jpmml-evaluator \
+      --add-flags "-jar $out/share/java/jpmml-evaluator.jar"
+
+    runHook postInstall
+  '';
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Java Evaluator API for PMML";
+    homepage = "https://github.com/jpmml/jpmml-evaluator";
+    changelog = "https://github.com/jpmml/jpmml-evaluator/releases/tag/${version}";
+    license = lib.licenses.agpl3Only;
+    maintainers = [ lib.maintainers.b-rodrigues ];
+    mainProgram = "jpmml-evaluator";
+    platforms = lib.platforms.all;
+    sourceProvenance = with lib.sourceTypes; [ fromSource ];
+  };
+}
