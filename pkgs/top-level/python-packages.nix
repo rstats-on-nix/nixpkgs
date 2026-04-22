064db3195cc580d2fc5eb8ddc6160b4485967b8d
Bruno Rodrigues <brodriguesco@protonmail.com>
2026-04-22 15:28:22 +0200

    python3Packages.quartodoc: init at 0.11.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16816,6 +16816,8 @@ self: super: with self; {
 
   quart-trio = callPackage ../development/python-modules/quart-trio { };
 
+  quartodoc = callPackage ../development/python-modules/quartodoc { };
+
   quaternion = callPackage ../development/python-modules/quaternion { };
 
   qudida = callPackage ../development/python-modules/qudida { };
