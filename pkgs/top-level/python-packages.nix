aec0b8e64c09d1ce902ab33a3669f324fdd39634
Bruno Rodrigues <brodriguesco@protonmail.com>
2026-04-22 15:27:48 +0200

    python3Packages.stdio-mgr: init at 1.0.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18712,6 +18712,8 @@ self: super: with self; {
 
   std2 = callPackage ../development/python-modules/std2 { };
 
+  stdio-mgr = callPackage ../development/python-modules/stdio-mgr { };
+
   stdiomask = callPackage ../development/python-modules/stdiomask { };
 
   stdlib-list = callPackage ../development/python-modules/stdlib-list { };
