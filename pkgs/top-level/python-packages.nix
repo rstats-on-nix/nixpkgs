baf6711af650c7f5c55014fbb4bbfc4c832f35b3
Bruno Rodrigues <brodriguesco@protonmail.com>
2026-04-22 15:28:00 +0200

    python3Packages.plum-dispatch: init at 2.8.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12682,6 +12682,8 @@ self: super: with self; {
 
   plum-py = callPackage ../development/python-modules/plum-py { };
 
+  plum-dispatch = callPackage ../development/python-modules/plum-dispatch { };
+
   plumbum = callPackage ../development/python-modules/plumbum { };
 
   pluralizer = callPackage ../development/python-modules/pluralizer { };
