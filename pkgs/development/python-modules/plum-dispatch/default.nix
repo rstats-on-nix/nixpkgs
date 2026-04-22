a04d256eced2e5bf55f27b7d69c8bb0ea2c0050e
Bruno Rodrigues <brodriguesco@protonmail.com>
2026-04-22 15:28:00 +0200

    python3Packages.plum-dispatch: init at 2.8.0

---
+++ pkgs/development/python-modules/plum-dispatch/default.nix
@@ -0,0 +1,57 @@
+{
+  lib,
+  beartype,
+  buildPythonPackage,
+  fetchPypi,
+  hatch-vcs,
+  hatchling,
+  ipython,
+  numpy,
+  pythonOlder,
+  rich,
+  sybil,
+  typing-extensions,
+  pytestCheckHook,
+}:
+
+buildPythonPackage rec {
+  pname = "plum-dispatch";
+  version = "2.8.0";
+  pyproject = true;
+
+  disabled = pythonOlder "3.10";
+
+  src = fetchPypi {
+    pname = "plum_dispatch";
+    inherit version;
+    hash = "sha256-RT/HvGfSo5SSyDSwDJTYFocdFItaCl0/SeK78jkeJhk=";
+  };
+
+  build-system = [
+    hatch-vcs
+    hatchling
+  ];
+
+  dependencies = [
+    beartype
+    rich
+    typing-extensions
+  ];
+
+  nativeCheckInputs = [
+    ipython
+    numpy
+    pytestCheckHook
+    sybil
+  ];
+
+  pythonImportsCheck = [ "plum" ];
+
+  meta = {
+    description = "Multiple dispatch in Python";
+    homepage = "https://github.com/wesselb/plum";
+    changelog = "https://github.com/wesselb/plum/releases/tag/v${version}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ ];
+  };
+}
