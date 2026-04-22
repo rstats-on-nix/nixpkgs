77168a686fca2990be9026951baa6c9d377ad15f
Bruno Rodrigues <brodriguesco@protonmail.com>
2026-04-22 15:28:11 +0200

    python3Packages.sphobjinv: init at 2.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -19048,6 +19048,8 @@ self: super: with self; {
 
   sphfile = callPackage ../development/python-modules/sphfile { };
 
+  sphobjinv = callPackage ../development/python-modules/sphobjinv { };
+
   sphinx = callPackage ../development/python-modules/sphinx { };
 
   sphinx-argparse = callPackage ../development/python-modules/sphinx-argparse { };
