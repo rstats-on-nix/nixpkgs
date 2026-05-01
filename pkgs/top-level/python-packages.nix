a96199732157343f212fcbb01b49bc5cf76dbb25
Bruno Rodrigues <bruno@brodrigues.co>
2026-05-01 12:53:24 +0000

    intermediate: libuv fix and second R package update

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -17235,6 +17235,7 @@ self: super: with self; {
   rxv = callPackage ../development/python-modules/rxv { };
 
   ryd-client = callPackage ../development/python-modules/ryd-client { };
+  ryxpress = callPackage ../development/python-modules/ryxpress { };
 
   rzpipe = callPackage ../development/python-modules/rzpipe { };
 
@@ -19045,6 +19046,7 @@ self: super: with self; {
   tailscale = callPackage ../development/python-modules/tailscale { };
 
   takethetime = callPackage ../development/python-modules/takethetime { };
+  talvez = callPackage ../development/python-modules/talvez { };
 
   tami4edgeapi = callPackage ../development/python-modules/tami4edgeapi { };
 
