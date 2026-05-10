1f03ac892df0c92e82f2a2491d8131710d8e8b79
Bruno Rodrigues <bruno@brodrigues.co>
2026-05-10 12:53:00 +0000

    intermediate: libuv fix and second R package update

--- pkgs/development/julia-modules/registry.nix
+++ pkgs/development/julia-modules/registry.nix
@@ -3,7 +3,7 @@
 fetchFromGitHub {
   owner = "CodeDownIO";
   repo = "General";
-  rev = "4b19a1dc55d2877e85a5d0e98702b75872210e9d";
-  sha256 = "sha256-mVeBTpEQnW3fvJu1+4T8z+earMjEgtdy0tnZnAxz/pk=";
+  rev = "0facfb158ea6e139212b9f86944dacf239c23b2a";
+  sha256 = "02c6mypqjb43ppg3cpmbfhdz1jzkd0aflac8jra0zr7xml3gkkz2";
   # date = "2025-08-12T05:20:40+00:00";
 }
