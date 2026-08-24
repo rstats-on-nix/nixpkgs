ece897712d1575133395002d37f4a758409bc452
Bruno Rodrigues <bruno@brodrigues.co>
2026-08-24 13:19:37 +0000

    daily update

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18416,6 +18416,7 @@ self: super: with self; {
   rxv = callPackage ../development/python-modules/rxv { };
 
   ryd-client = callPackage ../development/python-modules/ryd-client { };
+  ryxpress = callPackage ../development/python-modules/ryxpress { };
 
   rzpipe = callPackage ../development/python-modules/rzpipe { };
 
@@ -20307,6 +20308,7 @@ self: super: with self; {
   tailscale = callPackage ../development/python-modules/tailscale { };
 
   takethetime = callPackage ../development/python-modules/takethetime { };
+  talvez = callPackage ../development/python-modules/talvez { };
 
   tami4edgeapi = callPackage ../development/python-modules/tami4edgeapi { };
 
