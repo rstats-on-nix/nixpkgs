ac7d12fa9cdb3339ee058f77fcd77fce58658bdd
Bruno Rodrigues <bruno@brodrigues.co>
2026-09-09 16:37:34 +0000

    daily update

--- pkgs/development/r-modules/generate-r-packages.R
+++ pkgs/development/r-modules/generate-r-packages.R
@@ -46,24 +46,38 @@ escapeName <- function(name) {
 
 nixPrefetch <- function(name, version) {
   prevPkg <- prevPkgs[[escapeName(name)]]
-  if (!is.null(prevPkg) && prevPkg$version == version)
-    prevPkg$sha256
-
-  else {
-    # avoid nix-prefetch-url because it often fails to fetch/hash large files
+  if (!is.null(prevPkg) && prevPkg$version == version) {
+    hash <- if (is.null(prevPkg$sha256)) NA_character_ else as.character(prevPkg$sha256)
+    return(c(hash=hash, version=as.character(version)))
+  } else {
     url <- paste0(mirrorUrl, name, "_", version, ".tar.gz")
     tmp <- tempfile(pattern=paste0(name, "_", version), fileext=".tar.gz")
-    cmd <- paste0("wget -q -O '", tmp, "' '", url, "'")
+    cmd1 <- paste0("wget -q -O '", tmp, "' '", url, "'")
     if(mirrorType == "cran"){
       archiveUrl <- paste0(mirrorUrl, "Archive/", name, "/", name, "_", version, ".tar.gz")
-      cmd <- paste0(cmd, " || wget -q -O '", tmp, "' '", archiveUrl, "'")
+      cmd1 <- paste0(cmd1, " || wget -q -O '", tmp, "' '", archiveUrl, "'")
     }
-    cmd <- paste0(cmd, " && nix-hash --type sha256 --base32 --flat '", tmp, "'")
-    cmd <- paste0(cmd, " && echo >&2 '  added ", name, " v", version, "'")
-    cmd <- paste0(cmd, " ; rm -rf '", tmp, "'")
-    system(cmd, intern=TRUE)
+    sys_res <- system(cmd1)
+    if (sys_res != 0 && mirrorType == "cran" && grepl("-[0-9]+$", version)) {
+       base_version <- sub("-[0-9]+$", "", version)
+       url_base <- paste0(mirrorUrl, name, "_", base_version, ".tar.gz")
+       archive_base <- paste0(mirrorUrl, "Archive/", name, "/", name, "_", base_version, ".tar.gz")
+       cmd2 <- paste0("wget -q -O '", tmp, "' '", url_base, "' || wget -q -O '", tmp, "' '", archive_base, "'")
+       if (system(cmd2) == 0) {
+           version <- base_version
+           sys_res <- 0
+       }
+    }
+    if (sys_res != 0) return(c(hash=NA_character_, version=as.character(version)))
+    cmd_hash <- paste0("nix-hash --type sha256 --base32 --flat '", tmp, "'")
+    res <- system(cmd_hash, intern=TRUE)
+    res <- res[nzchar(res)]
+    unlink(tmp)
+    hash <- if (length(res) == 0) NA_character_ else res[length(res)]
+    echo_cmd <- paste0("echo >&2 '  added ", name, " v", version, "'")
+    system(echo_cmd)
+    return(c(hash=as.character(hash), version=as.character(version)))
   }
-
 }
 
 formatPackage <- function(name, version, sha256, depends, imports, linkingTo) {
@@ -86,7 +100,9 @@ cl <- makeCluster(10)
 clusterExport(cl, c("escapeName", "nixPrefetch", "prevPkgs", "mirrorUrl", "mirrorType", "knownPackageNames"))
 
 write(paste("updating", mirrorType, "packages"), stderr())
-pkgTable$sha256 <- parApply(cl, pkgTable, 1, function(p) nixPrefetch(p[1], p[2]))
+prefetch_res <- parApply(cl, pkgTable, 1, function(p) nixPrefetch(p[1], p[2]))
+pkgTable$sha256 <- prefetch_res["hash", ]
+pkgTable$Version <- prefetch_res["version", ]
 
 stopCluster(cl)
 
